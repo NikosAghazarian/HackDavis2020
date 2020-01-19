f633d2c90d727ed8e235c9e898d6fc4949edadc6
Nikos Aghazarian <odst46@gmail.com>
2020-01-18 17:43:29 -0800

    clean up stray imgs on start

--- server/main.js
+++ server/main.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
 const router = require('./router-component.js');
 
 
 let app = express();
+
+const directory = path.join(__dirname + '/temp_images/'); // this mess cleans the temp_images if the program quit before its removal callback
+fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+  
+    for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+            if (err) throw err;
+        });
+    }
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
--- server/router-component.js
+++ server/router-component.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const exec = require('child_process');
 const uuidv4 = require('uuid/v4');
 
 const router = require('express').Router();
@@ -20,19 +21,21 @@ router.post('/upload/post', upload.single('image'), (req, res, err) => {
         res.status(401).json({error: 'Please provide an image'});
     }
 
-    const TTL = 120; //time for images to live in seconds
+    const TTL = 120; //time to live for images, in seconds
     const filename = uuidv4() + '.png';
     const imagePath = path.join(__dirname, `/temp_images/${filename}`);
 
     fs.writeFileSync(imagePath, req.file.buffer);
+    console.log(`File ${filename} has been created and will be stored for ${TTL} seconds locally before deletion.`);
     setTimeout( () => {
         fs.unlink(imagePath, (err) => {
             if (err) throw err;
-            console.log(`File ${filename} was deleted after 100 seconds`);
+            console.log(`File ${filename} was deleted.`);
         });
     }, TTL*1000);
 
     res.status(200).json({ name: filename });
+    //need to send page data after api responses
 });
 
 module.exports = router
